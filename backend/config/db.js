032239f39e6e1e56bfa8a70d5904d27aa244e474
Minh Dang <dangtienminh834@gmail.com>
2025-12-03 00:47:01 +0700

    fixed config and new mongoDB string

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
-
+ 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: "wastewise"
+    });
+ 
+    console.log("MongoDB Connected:", mongoose.connection.host);
+  }
+  catch (error) {
+    console.error("MongoDB Connection Error:", error);
     process.exit(1);
   }
 };
-
+ 
 module.exports = connectDB;
